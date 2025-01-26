78403e399d54a67ddb0b11af7d040669503e7d1c
Aman Tiwari <ammitiwari1234@gmail.com>
2025-01-26 16:01:30 +0530

    Hardcoded origin

--- server/routes/router.go
+++ server/routes/router.go
@@ -9,11 +9,11 @@ func Router() *gin.Engine {
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},                   // Specify allowed origins
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}, // Specify allowed methods
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"}, // Specify allowed headers
-		ExposeHeaders:    []string{"Content-Length"},                          // Specify exposed headers
-		AllowCredentials: true,                                                // Allow credentials (cookies, authorization headers)
+		AllowOrigins:     []string{"http://localhost:3000", "https://cdc-transformer-go.vercel.app"}, // Specify allowed origins
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},                        // Specify allowed methods
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},                        // Specify allowed headers
+		ExposeHeaders:    []string{"Content-Length"},                                                 // Specify exposed headers
+		AllowCredentials: true,                                                                       // Allow credentials (cookies, authorization headers)
 	}))
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{"message": "Welcome to CDC"})
