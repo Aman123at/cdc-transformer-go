2db08d8fa438269cff96f509567b05d3a90fb8d0
Aman Tiwari <ammitiwari1234@gmail.com>
2025-02-01 10:26:28 +0530

    added new logo

--- client/src/components/custom/Header.tsx
+++ client/src/components/custom/Header.tsx
@@ -1,12 +1,16 @@
 'use client'
 import { Button } from '@/components/ui/button'
 import { ClipboardList } from 'lucide-react'
+import Image from 'next/image'
+import logo from '../../../assets/CDC_LOGO.png'
 
 export default function Header({ onCheckLogs }: { onCheckLogs: () => void }) {
   return (
     <header className="flex justify-between items-center p-4 bg-secondary">
       <div className="flex items-center">
-        <div className="w-10 h-10 bg-primary rounded-full mr-2"></div>
+        <div className="w-10 h-10 bg-primary rounded-full mr-2">
+          <Image src={logo} alt='logo' />
+        </div>
         <span className="text-xl font-bold">CDC Service</span>
       </div>
       <Button onClick={onCheckLogs} variant="outline">
